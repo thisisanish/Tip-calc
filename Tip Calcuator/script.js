9eb7e8373492b13a42662626e6217abcc7ddbf1d
Anish Agarwal <34007888+thisisanish@users.noreply.github.com>
2018-02-14 00:25:22 +0530

    Add files via upload

---
+++ Tip Calcuator/script.js
@@ -0,0 +1,42 @@
+// OHH yeahhh lets go
+//function goes here
+function calcuateTip(){
+//storing the data into the variables
+var billAmount=document.getElementById("billAmount").value;
+var serviceQuality = document.getElementById("serviceQuality").value;
+var numPeople = document.getElementById("totalPeople").value;
+
+// Quick Validation
+
+if (billAmount === "" || serviceQuality === 0) {
+  window.alert("Please Enter some values to get this baby up and running!");
+  return; //returns to the same function and stops it from contuining
+}
+// check to see if the input is  or less thant or equal to 1
+  if (numPeople==="" || numPeople <= 1) {
+    numPeople = 1;
+    document.getElementById("each").style.display="none"; //hides ease or that eleent
+  }
+  else {
+    document.getElementById("each").style.display ="block";
+  }
+
+  // some bitchy maths
+  var total = (billAmount*serviceQuality)/numPeople;
+  window.alert(total);
+}
+
+}
+
+
+
+
+//hide the tip Amount
+document.getElementById("totalTip").style.display="none";
+document.getElementById("each").style.display="none";
+
+// clicking the button calls my custom function;calcuateTip
+
+document.getElementById("calcuate").onclick = function(){
+  calcuateTip();
+}
